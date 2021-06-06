e70187998a88dc8fbea468df0283be3d1fe377d6
KeyJ148 <kejik148@gmail.com>
2021-06-06 19:46:12 +0300

    Small refactoring last commit

--- src/main/java/cc/abro/orchengine/gameobject/components/gui/EventableGuiElement.java
+++ src/main/java/cc/abro/orchengine/gameobject/components/gui/EventableGuiElement.java
@@ -6,34 +6,24 @@ import org.liquidengine.legui.component.Component;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public abstract class EventableGuiElement<T extends Component> extends GuiElement<T> {
 
     private final Map<Class<? extends GuiElementController>, GuiElementController> controllerByClass = new HashMap<>();
 
-    public EventableGuiElement(T component, List<GuiElementController> controllers) {
+    public EventableGuiElement(T component, Set<GuiElementController> controllers) {
         super(component);
         initControllers(controllers);
     }
 
-    public EventableGuiElement(T component, List<GuiElementController> controllers,
+    public EventableGuiElement(T component, Set<GuiElementController> controllers,
                                boolean moveComponentToGameObjectPosition) {
         super(component, moveComponentToGameObjectPosition);
         initControllers(controllers);
     }
 
-    public EventableGuiElement(T component, List<GuiElementController> controllers, int weight, int height) {
-        super(component, weight, height);
-        initControllers(controllers);
-    }
-
-    public EventableGuiElement(T component, List<GuiElementController> controllers,
-                               boolean moveComponentToGameObjectPosition, int weight, int height) {
-        super(component, moveComponentToGameObjectPosition, weight, height);
-        initControllers(controllers);
-    }
-
-    private void initControllers(List<GuiElementController> controllers){
+    private void initControllers(Set<GuiElementController> controllers){
         for (GuiElementController controller : controllers){
             controller.init(this);
             controllerByClass.put(controller.getClass(), controller);
--- src/main/java/cc/abro/orchengine/gameobject/components/gui/EventableGuiPanelElement.java
+++ src/main/java/cc/abro/orchengine/gameobject/components/gui/EventableGuiPanelElement.java
@@ -4,26 +4,17 @@ import cc.abro.orchengine.gui.EventableGuiPanel;
 import org.liquidengine.legui.component.Component;
 
 import java.util.List;
+import java.util.Set;
 
 public class EventableGuiPanelElement<T extends EventableGuiPanel>  extends EventableGuiElement<T>{
 
-    public EventableGuiPanelElement(T component, List<GuiElementController> controllers) {
+    public EventableGuiPanelElement(T component, Set<GuiElementController> controllers) {
         super(component, controllers);
         component.addListener(this);
     }
 
-    public EventableGuiPanelElement(T component, List<GuiElementController> controllers, boolean moveComponentToGameObjectPosition) {
+    public EventableGuiPanelElement(T component, Set<GuiElementController> controllers, boolean moveComponentToGameObjectPosition) {
         super(component, controllers, moveComponentToGameObjectPosition);
-        component.addListener(this);
-    }
-
-    public EventableGuiPanelElement(T component, List<GuiElementController> controllers, int weight, int height) {
-        super(component, controllers, weight, height);
-        component.addListener(this);
-    }
-
-    public EventableGuiPanelElement(T component, List<GuiElementController> controllers, boolean moveComponentToGameObjectPosition, int weight, int height) {
-        super(component, controllers, moveComponentToGameObjectPosition, weight, height);
         component.addListener(this);
     }
 }
